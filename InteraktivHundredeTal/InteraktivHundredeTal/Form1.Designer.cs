7560742b2416ca9393b943836272354c3c42f3f5
Le-Holding <68299863+Le-Holding@users.noreply.github.com>
2024-10-25 23:37:07 +0200

    Change textboxes position order

--- InteraktivHundredeTal/InteraktivHundredeTal/Form1.Designer.cs
+++ InteraktivHundredeTal/InteraktivHundredeTal/Form1.Designer.cs
@@ -71,7 +71,7 @@ namespace InteraktivHundredeTal
             // textBox_AddTen
             // 
             this.textBox_AddTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.textBox_AddTen.Location = new System.Drawing.Point(244, 96);
+            this.textBox_AddTen.Location = new System.Drawing.Point(244, 193);
             this.textBox_AddTen.Name = "textBox_AddTen";
             this.textBox_AddTen.Size = new System.Drawing.Size(100, 49);
             this.textBox_AddTen.TabIndex = 1;
@@ -95,7 +95,7 @@ namespace InteraktivHundredeTal
             // textBox_SubTen
             // 
             this.textBox_SubTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.textBox_SubTen.Location = new System.Drawing.Point(244, 193);
+            this.textBox_SubTen.Location = new System.Drawing.Point(244, 96);
             this.textBox_SubTen.Name = "textBox_SubTen";
             this.textBox_SubTen.Size = new System.Drawing.Size(100, 49);
             this.textBox_SubTen.TabIndex = 4;
@@ -103,7 +103,7 @@ namespace InteraktivHundredeTal
             // textBox_Sub20
             // 
             this.textBox_Sub20.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.textBox_Sub20.Location = new System.Drawing.Point(244, 269);
+            this.textBox_Sub20.Location = new System.Drawing.Point(244, 19);
             this.textBox_Sub20.Name = "textBox_Sub20";
             this.textBox_Sub20.Size = new System.Drawing.Size(100, 49);
             this.textBox_Sub20.TabIndex = 5;
@@ -111,7 +111,7 @@ namespace InteraktivHundredeTal
             // textBox_Add20
             // 
             this.textBox_Add20.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.textBox_Add20.Location = new System.Drawing.Point(244, 19);
+            this.textBox_Add20.Location = new System.Drawing.Point(244, 269);
             this.textBox_Add20.Name = "textBox_Add20";
             this.textBox_Add20.Size = new System.Drawing.Size(100, 49);
             this.textBox_Add20.TabIndex = 6;
@@ -155,7 +155,7 @@ namespace InteraktivHundredeTal
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(276, 73);
+            this.label2.Location = new System.Drawing.Point(276, 245);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 20);
             this.label2.TabIndex = 11;
@@ -167,7 +167,7 @@ namespace InteraktivHundredeTal
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(419, 125);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.Size = new System.Drawing.Size(35, 20);
             this.label3.TabIndex = 12;
             this.label3.Text = "+1";
             // 
@@ -177,7 +177,7 @@ namespace InteraktivHundredeTal
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(140, 197);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.Size = new System.Drawing.Size(35, 20);
             this.label4.TabIndex = 13;
             this.label4.Text = "-1";
             // 
@@ -185,7 +185,7 @@ namespace InteraktivHundredeTal
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(276, 245);
+            this.label5.Location = new System.Drawing.Point(276, 73);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 20);
             this.label5.TabIndex = 14;
@@ -195,7 +195,7 @@ namespace InteraktivHundredeTal
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(280, 321);
+            this.label6.Location = new System.Drawing.Point(276, -4);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(35, 20);
             this.label6.TabIndex = 15;
@@ -205,9 +205,9 @@ namespace InteraktivHundredeTal
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(276, -4);
+            this.label7.Location = new System.Drawing.Point(276, 321);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(39, 20);
+            this.label7.Size = new System.Drawing.Size(35, 20);
             this.label7.TabIndex = 16;
             this.label7.Text = "+20";
             // 
@@ -217,7 +217,7 @@ namespace InteraktivHundredeTal
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(533, 125);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(29, 20);
+            this.label8.Size = new System.Drawing.Size(35, 20);
             this.label8.TabIndex = 17;
             this.label8.Text = "+2";
             // 
@@ -227,7 +227,7 @@ namespace InteraktivHundredeTal
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(13, 197);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(25, 20);
+            this.label9.Size = new System.Drawing.Size(35, 20);
             this.label9.TabIndex = 18;
             this.label9.Text = "-2";
             // 
@@ -249,7 +249,7 @@ namespace InteraktivHundredeTal
             this.dataGridView1.Location = new System.Drawing.Point(603, 28);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(398, 246);
+            this.dataGridView1.Size = new System.Drawing.Size(398, 290);
             this.dataGridView1.TabIndex = 20;
             // 
             // checkBox1
@@ -257,7 +257,7 @@ namespace InteraktivHundredeTal
             this.checkBox1.AutoSize = true;
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(603, 300);
+            this.checkBox1.Location = new System.Drawing.Point(355, 295);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(101, 17);
             this.checkBox1.TabIndex = 21;
